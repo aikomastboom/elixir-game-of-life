6456c7a8234ac1354482b5ae6cded11402341965
Aiko Mastboom <git@aiko.sh>
2022-10-07 00:25:45 +0200

    updated so flip_cell can be called

--- lib/components.ex
+++ lib/components.ex
@@ -8,19 +8,19 @@ defmodule GameOfLife.Components do
   def world(assigns) do
     ~H"""
     <table>
-      <%= for r <- @world do %>
+      <%= for {r,x} <- Enum.with_index(@world) do %>
         <tr height="32">
-          <%= for c <- r do %>
+          <%= for {c,y} <- Enum.with_index(r) do %>
             <%= if c == 1 do %>
               <td width="32" class="bg-blue-500 align-middle text-center">
                 <b>
-                  <%= render_slot(@alive) || "@" %>
+                  <%= render_slot(@alive, [x, y]) || "@" %>
                 </b>
               </td>
             <% else %>
               <td width="32" class="bg-red-500 align-middle text-center">
                 <b>
-                  <%= render_slot(@dead) || "\u00B7" %>
+                  <%= render_slot(@dead, [x, y]) || "\u00B7" %>
                 </b>
               </td>
             <% end %>
